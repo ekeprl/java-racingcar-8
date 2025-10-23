5d2f7aa01d247474f1194535b16c97f40c516091
KUN <yoonkun1902@gmail.com>
2025-10-23 16:55:18 +0900

    feat(controller): RacingController에서 입력 처리 추가
    RacingController.run()에서 입력 처리 추가:
    - InputView.getCarNames()를 통해 자동차 이름 입력
    - InputView.getTryCount()를 통해 시도 횟수 입력

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,11 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+
+public class RacingController {
+    public void run() {
+        var cars = InputView.getCarNames();
+        var tryCount = InputView.getTryCount();
+
+    }
+}
