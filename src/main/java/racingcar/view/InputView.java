4944e55995d93c1a83b77fcc34560ebc14ff4623
KUN <yoonkun1902@gmail.com>
2025-10-23 17:14:31 +0900

    feat(inputview): 자동차 이름, 시행 횟수를 입력하기 위한
    -자동차 이름을 입력하는 getCarNames()
      하위에 각각 비어있는 값, 최대길이, 쉼표(,)로 구분 기능 추가
    -시행 횟수를 입력하는 getTryCount()
      하위에 비어있는 값, 음수체크 기능 추가
    -각 예외에 맞게 ErrorMessage에서 가져온 메시지를 출력할 수 있도록 작성.

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,77 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.exception.ErrorMessage;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class InputView {
+    public static List<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String input = Console.readLine();
+
+        validateInput(input);
+        splitInput(input);
+
+        return Collections.singletonList(input);
+
+    }
+
+    //글자 수 체크
+    private static void wordCountCheck(List<String> obj) {
+        for (String names : obj) {
+            if (names.length() > 5) {
+                throw new IllegalArgumentException(ErrorMessage.INVALID_OVER_CAR_NAME.getMessage());
+            }
+        }
+    }
+
+    //입력값 split
+    private static void splitInput(String obj) {
+
+        List<String> carNames = Arrays.stream(obj.split(","))
+                .map(String::trim)
+                .toList();
+
+        wordCountCheck(carNames);
+    }
+
+    //입력값 검증
+    private static void validateInput(String obj) {
+        if(obj == null || obj.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ZERO_CAR_NAME.getMessage());
+        }
+    }
+
+
+
+    //시행횟수 block
+    //입력값 검증
+    private static void inputCountCheck(int obj) {
+        if(obj <= 0 ) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_OF_ROUNDS.getMessage());
+        }
+    }
+
+    //입력값 검증
+    private static int validateCountInput(String obj) {
+        if(obj == null || obj.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ZERO_ROUND.getMessage());
+        }
+        int count = Integer.parseInt(obj);
+        inputCountCheck(count);
+        return count;
+
+    }
+
+    public static int getTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String input = Console.readLine();
+
+        return validateCountInput(input);
+    }
+
+}
