a7a0f8f300c9230e85570e8460131f777bb112b1
KUN <yoonkun1902@gmail.com>
2025-10-27 15:11:40 +0900

    feat(controller): 레이싱게임 컨트롤러 구현
    - 사용자 입력(InputView): 자동차 이름과 시도 횟수를 받아옴
    - 입력값으로 RacingCar 객체를 생성하고 RacingGame에 등록
    - 입력받은 횟수만큼 moveAll() 호출 & 라운드별 결과 출력
    - 최종 우승자 출력
    - 난수 생성은 Util에서 처리하도록 주입

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,11 +1,31 @@
 package racingcar.controller;
 
+import racingcar.model.RacingCar;
+import racingcar.model.RacingGame;
+import racingcar.util.Util;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingController {
+
     public void run() {
-        var cars = InputView.getCarNames();
+        Util util = new Util();
+
+        var carNames = InputView.getCarNames();
         var tryCount = InputView.getTryCount();
 
+        var carList = carNames.stream()
+                .map(name -> new RacingCar(util, name)) //util에서 난수 생성 처리
+                .toList();
+        RacingGame racingCars = new RacingGame(carList);
+
+        //시행횟수만큼 실행
+        for (int i = 0; i < tryCount; i++) {
+            racingCars.moveAll();
+            OutputView.printRoundResult(racingCars);
+        }
+
+        //결과출력
+        OutputView.printWinners(racingCars.findWinners());
     }
 }
